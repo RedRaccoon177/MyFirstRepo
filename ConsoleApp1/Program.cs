4be8f98b21cd3fd28c146871d4b937a9edfba970
RedRaccoon177 <145746284+RedRaccoon177@users.noreply.github.com>
2024-12-26 21:33:00 +0900

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,62 +4,30 @@ namespace ConsoleApp1
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            // 입력된 숫자를 누적 저장할 변수
-            string temp = "";
+            int sd = 0;
+            GetInput(ref sd);
 
-             int son= GetNumericInput(temp);
-
-            Console.WriteLine($"숫자 인트: {son}");
-
+            Console.WriteLine(sd);
         }
 
-        // 숫자 입력 처리 함수
-        public static int GetNumericInput(string temp)
+        public static void GetInput(ref int getNum0)
         {
-            while (true)
+            //문자열 일경우 제외
+            //숫자인데 0일경우 제외
+            bool isNum = false;
+            int getNum1 = 0;
+
+            while (isNum == false && getNum1 == 0)
             {
-                // 키 입력 받기
-                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
-
-                //0~9의 값을 받을 경우 tenp
-                if (keyInfo.Key == ConsoleKey.D0) { temp += +0; }
-                else if (keyInfo.Key == ConsoleKey.D1) { temp += +1; }
-                else if (keyInfo.Key == ConsoleKey.D2) { temp += +2; }
-                else if (keyInfo.Key == ConsoleKey.D3) { temp += +3; }
-                else if (keyInfo.Key == ConsoleKey.D4) { temp += +4; }
-                else if (keyInfo.Key == ConsoleKey.D5) { temp += +5; }
-                else if (keyInfo.Key == ConsoleKey.D6) { temp += +6; }
-                else if (keyInfo.Key == ConsoleKey.D7) { temp += +7; }
-                else if (keyInfo.Key == ConsoleKey.D8) { temp += +8; }
-                else if (keyInfo.Key == ConsoleKey.D9) { temp += +9; }
-                else if (keyInfo.Key == ConsoleKey.Enter) { break; }
-                else if (keyInfo.Key == ConsoleKey.Backspace)
-                {
-                    if (temp.Length > 0)
-                    {
-                        temp = temp.Substring(0, temp.Length - 1); // 마지막 문자 제거
-                        Console.Write("\b \b"); // 콘솔에서 문자 삭제
-                    }
-                }
-
-                // 숫자 키인지 확인
-                if (char.IsDigit(keyInfo.KeyChar))
-                {
-                    Console.Write(keyInfo.KeyChar); // 화면에 표시
-                }
+                isNum = int.TryParse(Console.ReadLine(), out getNum0);
+                getNum1 = getNum0;
             }
 
-            if (int.TryParse(temp, out int result))
-            {
-                return result; // 변환된 정수 반환
-            }
-            else
-            {
-                Console.WriteLine("\n유효한 숫자가 입력되지 않았습니다. 0으로 반환합니다.");
-                return 0; // 유효하지 않으면 0 반환
-            }
+            getNum0 = getNum1;
+
         }
     }
 }
